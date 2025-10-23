ef105798e571b6ddbb2c5a96c54b57495f61c229
Duck <limwoongtae@gmail.com>
2025-10-23 10:30:18 +0900

    temp push

--- src/main/java/kr/ac/collage_api/config/SecurityConfig.java
+++ src/main/java/kr/ac/collage_api/config/SecurityConfig.java
@@ -6,15 +6,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    public WebSecurityCustomizer configure() {
+        return web -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/static/**"));
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(hbasic -> hbasic.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 );
---
+++ src/main/java/kr/ac/collage_api/security/vo/AcntVO.java
@@ -0,0 +1,14 @@
+package kr.ac.collage_api.security.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class AcntVO {
+        private
+}
