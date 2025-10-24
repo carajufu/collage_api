e154d629b6732d68a330b270e3b73b320fdd37d1
Duck <limwoongtae@gmail.com>
2025-10-24 15:29:34 +0900

    AcntVO conflict resolve

--- src/main/java/kr/ac/collage_api/vo/AcntVO.java
+++ src/main/java/kr/ac/collage_api/vo/AcntVO.java
@@ -1,21 +1,5 @@
 package kr.ac.collage_api.vo;
 
-<<<<<<< HEAD
-import lombok.Data;
-
-import java.util.List;
-
-//계정
-@Data
-public class AcntVO {
-	private String acntId;   //계정_ID(PK)
-	private String password; //비밀번호
-	private String acntTy;   //계정_유형
-    private String fileGroupNo;
-
-    private List<AuthorVO> authorVOList;
-}
-=======
 import java.util.List;
 
 import lombok.Data;
@@ -31,6 +15,5 @@ public class AcntVO {
 
 	private List<AuthorVO> authorVOList;
 }
->>>>>>> 065f1d3a4570b58f75c1d1a9fc72716128730d4b
 
 
