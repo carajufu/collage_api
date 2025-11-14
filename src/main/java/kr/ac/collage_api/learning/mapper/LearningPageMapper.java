01451906768e0102190032bc487c08d1ad8f6947
Duck <limwoongtae@gmail.com>
2025-11-14 11:03:52 +0900

    commit before checkout to syw

--- src/main/java/kr/ac/collage_api/learning/mapper/LearningPageMapper.java
+++ src/main/java/kr/ac/collage_api/learning/mapper/LearningPageMapper.java
@@ -1,6 +1,6 @@
 package kr.ac.collage_api.learning.mapper;
 
-import kr.ac.collage_api.dashboard.vo.TaskPresentnVO;
+import kr.ac.collage_api.learning.vo.TaskPresentnVO;
 import kr.ac.collage_api.learning.vo.TaskVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -12,12 +12,12 @@ import java.util.Map;
 public interface LearningPageMapper {
     List<Map<String, Object>> getLearningPage(String lecNo);
 
+    Map<String, Object> getLectureInfo(String lecNo);
+
     List<TaskVO> taskList(String lecNo, String weekNo);
 
     TaskPresentnVO getSubmitTask(String taskNo, String studentNo);
 
     int taskFileUpload(@Param("taskpresentnNo") String taskpresentnNo,
                           @Param("fileGroupNo") Long fileGroupNo);
-
-    Map<String, Object> getLectureInfo(String lecNo);
 }
--- src/main/java/kr/ac/collage_api/learning/service/impl/LearningPageServiceImpl.java
+++ src/main/java/kr/ac/collage_api/learning/service/impl/LearningPageServiceImpl.java
@@ -1,7 +1,7 @@
 package kr.ac.collage_api.learning.service.impl;
 
 import kr.ac.collage_api.common.attach.service.UploadController;
-import kr.ac.collage_api.dashboard.vo.TaskPresentnVO;
+import kr.ac.collage_api.learning.vo.TaskPresentnVO;
 import kr.ac.collage_api.learning.vo.TaskVO;
 import kr.ac.collage_api.learning.mapper.LearningPageMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,15 +21,21 @@ public class LearningPageServiceImpl {
     @Autowired
     UploadController uploadController;
 
-    public List<Map<String, Object>> getLearningPage(String lecNo) {
+    public Map<String, Object> getLearningPage(String lecNo) {
         List<Map<String, Object>> weekInfoList = learningPageMapper.getLearningPage(lecNo);
 
-        weekInfoList.add(learningPageMapper.getLectureInfo(lecNo));
+        Map<String, Object> lectureInfo = learningPageMapper.getLectureInfo(lecNo);
+
+        Map<String, Object> infoMap = new HashMap<>();
+        infoMap.put("weekList", weekInfoList);
+        infoMap.put("lecInfo", lectureInfo);
+
 
 //        todo: estbl_course의 profsr_no로 교수 정보 검색하기 null 체크 구현 필 estbl이 list에 어떻게 담기는지 확인 필요
 //        weekInfoList.add(learningPageMapper.getProfInfo())
 
-        return weekInfoList;
+        // todo: list 반환 말고 map으로 list와 교수 정보 담아서 보내야할듯
+        return infoMap;
     }
 
     public List<TaskVO> taskList(String lecNo, String weekNo) {
