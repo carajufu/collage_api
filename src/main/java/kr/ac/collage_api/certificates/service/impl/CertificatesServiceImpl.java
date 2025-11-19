b890aadfca2a7ae812622555140b927afbfde865
carajufu <limwoongtae@gmail.com>
2025-11-19 20:32:03 +0900

    Delete src/main/java/kr/ac/collage_api/certificates/service/impl/CertificatesServiceImpl.java

--- src/main/java/kr/ac/collage_api/certificates/service/impl/CertificatesServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.collage_api.certificates.service.impl;
-
-import kr.ac.collage_api.certificates.service.CertificatesService;
-
-public class CertificatesServiceImpl implements CertificatesService {
-
-}
